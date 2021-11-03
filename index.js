9a68bf5c7897d7b6ff4ea87984a75b8b3bfef278
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-03 15:29:54 +0100

    Added color to the displayed results

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+const chalk = require("chalk");
 
 let myNumbers = process.argv.slice(2); 
 let n1 = process.argv.slice(2)[0];
 let n2 = process.argv.slice(2)[1];
-console.log(n1);
 
 const Sumar = (n1,n2) => parseInt(n1) + parseInt(n2);
 const Restar = (n1,n2) => parseInt(n1) - parseInt(n2);
@@ -10,15 +10,16 @@ const Multiplicar = (n1,n2) => parseInt(n1) * parseInt(n2);
 const Dividir = (n1,n2) => parseInt(n1) / parseInt(n2);
 
 if (myNumbers.length < 2) {
-  console.log("Tienes que introducir 2 numeros")
+  console.log(chalk.blue("Tienes que introducir 2 numeros"))
 } else {
-console.log(
-`Resultados:
-Sumar: ${Sumar(n1,n2)}
-Restar: ${Restar(n1,n2)}
-Multiplicar: ${Multiplicar(n1,n2)}
-Dividir: ${Dividir(n1,n2)} 
-`)
+console.log(chalk.yellow(
+    `Resultados:
+    Sumar: ${Sumar(n1,n2)}
+    Restar: ${Restar(n1,n2)}
+    Multiplicar: ${Multiplicar(n1,n2)}
+    Dividir: ${Dividir(n1,n2)} 
+    `)
+  )
 };
 
 
