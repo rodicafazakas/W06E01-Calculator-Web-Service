de0110d6f168ca570ba86159e78d3de4821e911f
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-03 15:26:18 +0100

    Added condition of having 2 numbers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+let myNumbers = process.argv.slice(2); 
 let n1 = process.argv.slice(2)[0];
 let n2 = process.argv.slice(2)[1];
 console.log(n1);
@@ -7,6 +9,9 @@ const Restar = (n1,n2) => parseInt(n1) - parseInt(n2);
 const Multiplicar = (n1,n2) => parseInt(n1) * parseInt(n2);
 const Dividir = (n1,n2) => parseInt(n1) / parseInt(n2);
 
+if (myNumbers.length < 2) {
+  console.log("Tienes que introducir 2 numeros")
+} else {
 console.log(
 `Resultados:
 Sumar: ${Sumar(n1,n2)}
@@ -14,6 +19,6 @@ Restar: ${Restar(n1,n2)}
 Multiplicar: ${Multiplicar(n1,n2)}
 Dividir: ${Dividir(n1,n2)} 
 `)
-
+};
 
 
