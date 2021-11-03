0bb50070f49618a8a08ee77e994ff59054807add
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-03 15:02:20 +0100

    Initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+const operaciones = {
+    sumar: (n1,n2) => n1 + n2,
+    restar: (n1,n2) => n1 - n2,
+    multilicar: (n1,n2) => n1 * n2,
+    dividir: (n1,n2) => n1 / n2
+} 
+
+let myNumbers = process.argv.slice(2);
+console.log(mynumbers);
