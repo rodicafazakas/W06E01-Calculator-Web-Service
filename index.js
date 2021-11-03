9c8da3f2012f3418f976929a83014ad0bff19888
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-03 15:21:24 +0100

    Defined operations

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const operaciones = {
-    sumar: (n1,n2) => n1 + n2,
-    restar: (n1,n2) => n1 - n2,
-    multilicar: (n1,n2) => n1 * n2,
-    dividir: (n1,n2) => n1 / n2
-} 
+let n1 = process.argv.slice(2)[0];
+let n2 = process.argv.slice(2)[1];
+console.log(n1);
 
-let myNumbers = process.argv.slice(2);
-console.log(mynumbers);
+const Sumar = (n1,n2) => parseInt(n1) + parseInt(n2);
+const Restar = (n1,n2) => parseInt(n1) - parseInt(n2);
+const Multiplicar = (n1,n2) => parseInt(n1) * parseInt(n2);
+const Dividir = (n1,n2) => parseInt(n1) / parseInt(n2);
+
+console.log(
+`Resultados:
+Sumar: ${Sumar(n1,n2)}
+Restar: ${Restar(n1,n2)}
+Multiplicar: ${Multiplicar(n1,n2)}
+Dividir: ${Dividir(n1,n2)} 
+`)
+
+
+
