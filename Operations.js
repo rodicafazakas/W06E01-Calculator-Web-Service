a661411f27cebae4d7e164f479a6040dcc924314
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-03 16:08:00 +0100

    Added the prompt functionality

---
+++ Operations.js
@@ -0,0 +1,11 @@
+const Sumar = (n1,n2) => parseInt(n1) + parseInt(n2);
+const Restar = (n1,n2) => parseInt(n1) - parseInt(n2);
+const Multiplicar = (n1,n2) => parseInt(n1) * parseInt(n2);
+const Dividir = (n1,n2) => parseInt(n1) / parseInt(n2);
+
+module.exports={
+  Sumar,
+  Restar,
+  Multiplicar,
+  Dividir,
+}
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const chalk = require("chalk");
+const prompt = require('prompt');
+const {Sumar, Restar, Multiplicar, Dividir} = require("./Operations.js");
 
 let myNumbers = process.argv.slice(2); 
 let n1 = process.argv.slice(2)[0];
 let n2 = process.argv.slice(2)[1];
 
-const Sumar = (n1,n2) => parseInt(n1) + parseInt(n2);
-const Restar = (n1,n2) => parseInt(n1) - parseInt(n2);
-const Multiplicar = (n1,n2) => parseInt(n1) * parseInt(n2);
-const Dividir = (n1,n2) => parseInt(n1) / parseInt(n2);
-
-if (myNumbers.length < 2) {
-  console.log(chalk.blue("Tienes que introducir 2 numeros"))
+if (myNumbers.length === 0) {
+  prompt.start();
+  prompt.get(['n1', 'n2']).then( ({n1 , n2}) => {
+    console.log(chalk.cyan(
+      `Resultados:
+      Numeros: ${n1}, ${n2}
+      Sumar: ${Sumar(n1,n2)}
+      Restar: ${Restar(n1,n2)}
+      Multiplicar: ${Multiplicar(n1,n2)}
+      Dividir: ${Dividir(n1,n2)} 
+      `)
+    )}
+  )  
+} else if (myNumbers.length < 2) {
+  console.log(chalk.blue("Tienes que introducir 2 numeros"));
 } else {
 console.log(chalk.yellow(
     `Resultados:
